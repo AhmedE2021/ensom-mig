50d619f9d8f5736a55822fee13b11972f3ade40c
Ahmed Elsaedy <89975214+AhmedE2021@users.noreply.github.com>
2022-12-08 13:29:52 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+import Hero from './components/Hero';
+import Infographics from './components/Infographics';
+import PostsSection from "./components/PostsSection"
+
+
+
+
+function App() {
+
+ 
+
+  return (
+    <div className="App">
+     
+ 
+
+      
+      <Hero/>  
+<Infographics/>
+
+      
+ 
+
+
+
+    </div>
+  );
+}
+
+export default App;
