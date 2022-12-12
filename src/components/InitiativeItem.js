85aa66d17c32ad7229aa3cceb31e88d4a7339f78
AhmedE2021 <89975214+AhmedE2021@users.noreply.github.com>
2022-12-12 23:55:32 +0100

    update

--- src/components/InitiativeItem.js
+++ src/components/InitiativeItem.js
@@ -7,7 +7,7 @@ export default function InitiativeItem({ initiative }) {
         <article  className="initiative-card">
         <img className="initiative-img" src={initiative.acf.image} alt={initiative.title.rendered} />
         <div className="initiative-content"> 
-        <p> {parse(initiative.content.rendered)}</p>
+        <p className="initiative-text"> {parse(initiative.content.rendered)}</p>
         </div>
         </article>       
   
