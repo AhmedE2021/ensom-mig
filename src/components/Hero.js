48b15b7101bdf1207e0892505cc0f2cd9777528e
sudo-apt-get-good <89986144+toldeveloper@users.noreply.github.com>
2022-12-12 14:15:08 +0100

    added gradient classes, changed to flexbox videoes, added text to videoes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -21,44 +21,51 @@ return (
 <div className='campaign-page NavSection'  id="hero">
 
 {/* ICON FOR SCROLL */}
-<div className='expandmore' > <a href='#campaign_videos' > <ExpandMoreIcon style={{ color:'white', transition:'all 0.5s', fontSize:'100px'}}/> </a>
-</div>
+{/* <div className='expandmore' > <a href='#campaign_videos' > <ExpandMoreIcon style={{ color:'white', transition:'all 0.5s', fontSize:'100px'}}/> </a>
+</div> */}
 
                   
 <section className='campaign-videos' id='campaign_videos'>
+       
+        {/* Green */}
         <div className='campaign-video'>
                 <video width="750" height="400" playsInline poster='http://talomensomhed.dk/wp-content/uploads/2022/12/yellowcard.png'  controls >
                         <source src='http://talomensomhed.dk/wp-content/uploads/2022/12/Yellow.mp4' type="video/mp4"/>
                 </video>
                 <div className='campaign-video-text'>
-                        <h1>Mental loneliness</h1>
+                        <h1 className='neon'>Ensomhed kan være når man ikke kender andre</h1>
+                        <h1 className='green-gradient campaign-video-text__description'>Alt var så anderledes end det, jeg kom fra</h1>
                 </div>
         </div>
 
 
+        {/* Blue */}
         <div className='campaign-video'>
                 <div className='campaign-video-text'>
-                        <h1>Isolation loneliness</h1>
+                        <h1 className='neon'>Ensomhed kan være når man er sammen med andre</h1>
+                        <h1 className='blue-gradient'>For det meste dukker følelsen op når jeg er sammen med andre</h1>
                 </div>
                 <video width="750" height="400" playsInline poster={greencard} controls >
                         <source src={greenvideo} type="video/mp4"/>
                 </video>
         </div>
 
-
+        {/* Yellow */}
         <div className='campaign-video'>
                 <video width="750" height="400" playsInline poster={purplecard}  controls >
                         <source src={purplevideo} type="video/mp4"/>
                 </video>
                 <div className='campaign-video-text'>
-                        <h1>Mental loneliness</h1>
+                        <h1 className='neon'>Ensomhed kan være når man ikke kender andre</h1>
+                        <h1 className='yellow-gradient'>Alt var så anderledes end det, jeg kom fra</h1>
                 </div> 
         </div>
 
-
+        {/* Purple */}
         <div className='campaign-video'>
                 <div className='campaign-video-text'>
-                        <h1>Mental loneliness</h1>
+                                        <h1 className='neon'>Ensomhed kan være når man ikke kender andre</h1>
+                                        <h1 className='purple-gradient'>Jeg følte mig aldrig som en del af flokken</h1>
                 </div>
                 <video width="750" height="400" playsInline poster={yellowcard}  controls >
                         <source src={yellowvideo} type="video/mp4"/>
