d2e6dbfca369ff8f52a461320c2aa2f14aef6350
sudo-apt-get-good <89986144+toldeveloper@users.noreply.github.com>
2022-12-13 12:11:15 +0100

    video section done, just lacks proper neon

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -5,7 +5,7 @@ import bluestill from '../assets/STILL_BLUE.jpg'
 export default function Homescreen() {
 
     return (
-      <>
+      <div className={styles.HomescreenComponent}>
         
 				<div className={styles.Homescreen}>
 					<div>
@@ -18,7 +18,7 @@ export default function Homescreen() {
           <img src={bluestill} alt="city" className={styles.cityIMG} />
           
           <div className={styles.textContainer}>
-            <h1 className={`neon ${styles.header}`}>Hvorfor taler vi ikke om det?</h1>
+            <h1 className={`neon--green ${styles.header}`}>Hvorfor taler vi ikke om det?</h1>
             <p>
               Det kan være svært at finde modet til at fortælle ens kæreste,
               familiemedlem eller kollega, at man føler sig ensom. Det skyldes
@@ -29,6 +29,6 @@ export default function Homescreen() {
           </div>
         </div>      
         
-			</>
+			</div>
 		);
 }
