40f44a4b74afc4e5610eaa78939971af9d6969f0
sudo-apt-get-good <89986144+toldeveloper@users.noreply.github.com>
2022-12-15 21:28:28 +0100

    cant remember

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,34 @@ import "./Footer.css"
 
 export default function Footer() {
 
-    return(
-        <section className="footer-section">
-
-         <div className="footer-message">
-          <p>Alle presserelaterede spørgsmål og henvendelser</p>
-          <p>vedrørende kampagnen bedes rettet til <span>laura.ensommig@gmail.com</span></p>  
-        </div>  
-         <div className="footer-icons">
-        <img className="footer-logo" src="http://quotes-api.talomensomhed.dk/wp-content/uploads/2022/12/VIA-Film-Transmedia-Logo-orange.png"  alt="via-film"/>
-        <img className="footer-logo" src="http://quotes-api.talomensomhed.dk/wp-content/uploads/2022/12/Aaarhus-Kommune-Logo-orange.png"  alt="aarhus-kommune"/>
-        <img className="footer-logo" src="http://quotes-api.talomensomhed.dk/wp-content/uploads/2022/12/Ensom-Mig-Logo-orange-smal.png"  alt="ensom-mig"/>
-        </div> 
-        </section>
-     
+    return (
+       <section className="footer-section">
+          <div>
+             <div className="footer-message">
+                <p>Alle presserelaterede spørgsmål og henvendelser</p>
+                <p>
+                   vedrørende kampagnen bedes rettet til{' '}
+                   <span>laura.ensommig@gmail.com</span>
+                </p>
+             </div>
+             <div className="footer-icons">
+                <img
+                   className="footer-logo"
+                   src="http://quotes-api.talomensomhed.dk/wp-content/uploads/2022/12/VIA-Film-_-Transmedia-Logo.png"
+                   alt="via-film"
+                />
+                <img
+                   className="footer-logo"
+                   src="http://quotes-api.talomensomhed.dk/wp-content/uploads/2022/12/Aaarhus-Kommune-Logo.png"
+                   alt="aarhus-kommune"
+                />
+                <img
+                   className="footer-logo"
+                   src="http://quotes-api.talomensomhed.dk/wp-content/uploads/2022/12/Ensom-Mig-Logo.png"
+                   alt="ensom-mig"
+                />
+             </div>
+          </div>
+       </section>
     )
 }
