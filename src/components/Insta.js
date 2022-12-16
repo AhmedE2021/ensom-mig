de8103b92bd057c683d90bf6054ae72f4c18e277
AhmedE2021 <89975214+AhmedE2021@users.noreply.github.com>
2022-12-16 12:41:09 +0100

    update

--- src/components/Insta.js
+++ src/components/Insta.js
@@ -26,19 +26,20 @@ export default function Insta () {
 
 <div className="insta-videos">
 
- <div style={{ justifyContent: 'center',margin:'1%',height:'100%' }}>
-  <InstagramEmbed url="https://www.instagram.com/p/COpW2_-hBU1/?utm_source=ig_web_copy_link"  height='91%' width='100%' captioned />
+ <div className="insta-vdieo">
+  <InstagramEmbed url="https://www.instagram.com/p/COpW2_-hBU1/?utm_source=ig_web_copy_link"  height='91%' width='100%' captioned/>
+ 
 </div>
 
-<div style={{  justifyContent: 'center',margin:'1%',height:'100%' }}>
+<div className="insta-vdieo">
   <InstagramEmbed url="https://www.instagram.com/p/CHC8eBFDiOb/?utm_source=ig_web_copy_link"  height='91%' width='100%' captioned />
 </div>
 
-<div style={{  justifyContent: 'center',margin:'1%',height:'100%' }}>
+<div  className="insta-vdieo">
   <InstagramEmbed url="https://www.instagram.com/p/COXtzYxBfQM/?utm_source=ig_web_copy_link" height='91%' width='100%' captioned />
 </div>
 
-<div style={{  justifyContent: 'center',margin:'1%',height:'100%' }}>
+<div className="insta-vdieo">
   <InstagramEmbed url="https://www.instagram.com/p/CHn15DyDYqF/?utm_source=ig_web_copy_link" height='91%' width='100%' captioned />
 </div>
 
